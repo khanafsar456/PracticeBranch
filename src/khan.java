16d85b1c929ca5224e82d47a33b7a05615de67bc
Khan Afsar <camrose2022@hotmail.com>
2022-12-20 20:45:22 -0800

    issue resolved

--- src/khan.java
+++ src/khan.java
@@ -1,7 +1,9 @@
 public class khan {
     public static void main(String[] args) {
         System.out.println("Edmonton");
-        String str=123;
+        String str="123";
+        int age=12;
+        
 
     }
 }
